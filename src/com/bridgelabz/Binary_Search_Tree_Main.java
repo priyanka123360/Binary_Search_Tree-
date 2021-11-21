ad97db5f26d510240a0432cb7cf5b5d06cf16e70
priyankap <priyanka123360@gmail.com>
2021-11-21 17:08:21 +0530

    Added : Binary Search Tree is Created.

--- src/com/bridgelabz/Binary_Search_Tree_Main.java
+++ src/com/bridgelabz/Binary_Search_Tree_Main.java
@@ -1,23 +1,44 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Binary_Search_Tree_Main {
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		Binary_Tree<Integer> binary_Tree = new Binary_Tree<>();
-		
+
 		binary_Tree.add(56);
 
 		binary_Tree.add(30);
 		binary_Tree.add(70);
-		
 		binary_Tree.add(22);
 		binary_Tree.add(40);
-		
+		binary_Tree.add(11);
+		binary_Tree.add(3);
+		binary_Tree.add(16);
 		binary_Tree.add(60);
 		binary_Tree.add(95);
-		
-		binary_Tree.inOrder();
-	}
-	
+		binary_Tree.add(65);
+		binary_Tree.add(63);
+		binary_Tree.add(67);
 
+		System.out.println("Enter 1 : For inOrder Binary tree \n Enter 2 : For pre order Binary tree \n Enter 3: For post order Binary tree");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		switch (num) {
+
+		case 1:
+			binary_Tree.inOrder();
+			break;
+		case 2:
+			binary_Tree.preOrder();
+			break;
+		case 3:
+			binary_Tree.postOrder();
+			break;
+		default:
+			System.out.println("Enter valid Number ");
+		}
+
+	}
 }
--- src/com/bridgelabz/Binary_Tree.java
+++ src/com/bridgelabz/Binary_Tree.java
@@ -1,45 +1,83 @@
 package com.bridgelabz;
 
 public class Binary_Tree<K extends Comparable<K>> {
-	
+
 	private My_Binary_Node<K> root;
-	
-	int count=0;
-	
+
+	int count = 0;
+
 	public void add(K key) {
-		
+
 		this.root = this.addRecursive(root, key);
 	}
 
 	private My_Binary_Node<K> addRecursive(My_Binary_Node<K> root, K key) {
-		
-		if(root == null) {
+
+		if (root == null) {
 			return new My_Binary_Node<>(key);
 		}
 		int comRes = key.compareTo(root.key);
-		if(comRes == 0) {
+		if (comRes == 0) {
 			return root;
 		}
-		if(comRes < 0) {
+		if (comRes < 0) {
 			root.left = addRecursive(root.left, key);
-		}else {
+		} else {
 			root.right = addRecursive(root.right, key);
 		}
 		return root;
 	}
-	
+
 	public void inOrder() {
-		
+
 		inOrderRec(this.root);
 	}
+
 	private void inOrderRec(My_Binary_Node<K> root) {
-		if(root != null) {
+		if (root != null) {
 
 			inOrderRec(root.left);
 			System.out.println(root.key);
 			inOrderRec(root.right);
 
 		}
+	}
+
+	public void preOrder() {
+
+		preOrderRec(this.root);
+	}
+
+	private void preOrderRec(My_Binary_Node<K> root2) {
+		// TODO Auto-generated method stub
+		if (root != null) {
+
+			System.out.println(root.key);
+
+			inOrderRec(root.left);
+			inOrderRec(root.right);
+
+		}
+
+	}
+
+	public void postOrder() {
+
+		postOrderRec(this.root);
+	}
+
+	private void postOrderRec(My_Binary_Node<K> root2) {
+		// TODO Auto-generated method stub
+		if (root != null) {
+
+			System.out.println(root.key);
+
+			inOrderRec(root.left);
+			inOrderRec(root.right);
+			System.out.println(root.key);
+
+		}
+
 	}
 
 }
